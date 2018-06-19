388e11882010d553049b9e9a3508baa8ccb2b657
Jordan Hoff <jhoff@linkedin.com>
2018-06-19 12:22:10 -0500

    Adds GeoLocation trait

---
+++ docs/geo_location.md
@@ -0,0 +1,89 @@
+# Geo Location
+
+The `GeoLocation` trait automatically maintains a [MySql Point](https://dev.mysql.com/doc/refman/5.7/en/gis-class-point.html) field based on an address. The `DistanceFrom` scope also allows you to sort records by distance from a provided point on the map.
+
+### Add a Point Field
+
+In a database migration, you will need to add a 'location' point field to your table.
+
+```
+DB::statement('ALTER TABLE customers ADD location POINT AFTER zipcode');
+```
+
+### Add the `GeoLocation` trait to your model
+
+The `GeoLocation` trait needs a `locationAddress` method defined on your model. This method should return a string that represents the mailing address that will be used to lookup the latitude and longitude.
+
+```php
+namespace App;
+
+use TwoThirds\EloquentTraits\GeoLocation;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use GeoLocation;
+
+    public function locationAddress() : string
+    {
+        return sprintf(
+            '%s %s, %s %s',
+            $this->address,
+            $this->city,
+            $this->state,
+            $this->zipcode
+        );
+    }
+    ...
+}
+```
+
+### Automatic maintenance of the location field
+
+Once setup, your model will automatically update the point field latitude and longitude whenever the value of `locationAddress` changes.
+
+```php
+$customer = App\Customer::find(1);
+
+$customer->address = '123 Main St.';
+
+$customer->save();
+```
+
+If you don't want the location to be updated, you can set the `$autoLocationUpdate` static property to `false`
+
+```php
+App\Customer::$autoLocationUpdate = true;
+```
+
+### Getting the latitude and longitude from the model
+
+Laravel doesn't know how to handle Point fields natively, so if you would like to get the values of the latitude and longitude, you can use the `withGeo` scope. This will make the location field output text.
+
+```php
+echo App\Customer::withGeo()->find(1)->location;
+// ["41.2812977", "-95.9424159"]
+
+echo App\Customer::find(1)->location;
+// null
+```
+
+### Setting latitude and longitude directly
+
+There is a setter on the `location` field that allows setting the field by string or array:
+
+```php
+$customer->location = ['41.2812977', '-95.9424159'];
+// or
+$customer->location = '41.2812977, -95.9424159';
+```
+
+### Sorting your records by distance
+
+The `distanceFrom` scope allows you to sort your records by distance from a point on the map. You can pass either an array or a string.
+
+```php
+App\Customer::distanceFrom(['41.2812977', '-95.9424159'])->get();
+// or
+App\Customer::distanceFrom('41.2812977, -95.9424159')->get();
+```
---
+++ tests/Unit/GeoLocationTest.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace TwoThirds\Testing\Unit;
+
+use phpmock\phpunit\PHPMock;
+use TwoThirds\Testing\TestCase;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Expression;
+use TwoThirds\EloquentTraits\GeoLocation;
+
+class GeoLocationTest extends TestCase
+{
+    use PHPMock;
+
+    /**
+     * @test
+     */
+    public function hasWithGeoScope()
+    {
+        $class = $this->getClass();
+
+        $builder = $class->withGeo()->applyScopes();
+
+        $this->assertContains(
+            'customers.*',
+            $builder->getQuery()->columns
+        );
+
+        $this->assertContains(
+            'astext(location) as location',
+            $builder->getQuery()->columns
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function returnsProperLocationAddress()
+    {
+        $class = $this->getClass([
+            'address' => '1234 Main street',
+        ]);
+
+        $this->assertEquals('1234 Main street is my address', $class->locationAddress());
+    }
+
+    /**
+     * @test
+     */
+    public function detectsChangesOnSaving()
+    {
+        $class = $this->getClass([
+            'address'  => '1234 Original street',
+            'location' => [10, 20],
+        ]);
+
+        // Pretend that this is coming from the database
+        $class->syncOriginal();
+
+        // Set a new address and presume that the google api will get called with it
+        $class->address = '2345 New street';
+        $this->googleApiShouldBeCalled($class, 'OK', [123, 234]);
+
+        $class->fireSaving();
+        $this->assertEquals([123, 234], $class->location);
+    }
+
+    /**
+     * @test
+     */
+    public function getsNullLocation()
+    {
+        $class = $this->getClass([
+            'location' => null,
+        ]);
+
+        $this->assertNull($class->location);
+    }
+
+    /**
+     * @test
+     */
+    public function parsesPointLocation()
+    {
+        $class = $this->getClass([
+            'location' => 'POINT(1234 2345)',
+        ]);
+
+        $this->assertEquals([1234, 2345], $class->location);
+    }
+
+    /**
+     * @test
+     */
+    public function setsArrayLocation()
+    {
+        $class = $this->getClass();
+
+        $class->location = [1234, 2345];
+
+        $location = $class->getAttributes()['location'];
+
+        $this->assertInstanceOf(Expression::class, $location);
+        $this->assertEquals('POINT(1234,2345)', $location->getValue());
+    }
+
+    /**
+     * @test
+     */
+    public function setsStringLocation()
+    {
+        $class = $this->getClass();
+
+        $class->location = 'POINT(1234,2345)';
+
+        $location = $class->getAttributes()['location'];
+
+        $this->assertEquals('POINT(1234,2345)', $location);
+    }
+
+    /**
+     * @test
+     */
+    public function scopeDistanceFromByString()
+    {
+        $builder = $this->getClass()
+            ->newQuery()
+            ->distanceFrom('1234,2345');
+
+        $this->assertInstanceOf(Expression::class, $builder->getQuery()->columns[2]);
+        $this->assertEquals('st_distance(location,POINT(1234,2345)) as distance', $builder->getQuery()->columns[2]->getValue());
+
+        $this->assertContains(
+            [
+                'type' => 'Raw',
+                'sql'  => '(0 - distance) DESC',
+            ],
+            $builder->getQuery()->orders
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function scopeDistanceFromByArray()
+    {
+        $builder = $this->getClass()
+            ->newQuery()
+            ->distanceFrom([1234, 2345]);
+
+        $this->assertInstanceOf(Expression::class, $builder->getQuery()->columns[2]);
+        $this->assertEquals('st_distance(location,POINT(1234,2345)) as distance', $builder->getQuery()->columns[2]->getValue());
+
+        $this->assertContains(
+            [
+                'type' => 'Raw',
+                'sql'  => '(0 - distance) DESC',
+            ],
+            $builder->getQuery()->orders
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function locationUpdateCanBeDisabled()
+    {
+        $model = $this->getClass([
+            'location' => 'foobar',
+        ]);
+
+        $this->googleApiShouldntBeCalled();
+
+        $model::$autoLocationUpdate = false;
+
+        $model->updateLocation();
+    }
+
+    /**
+     * Return a properly configured class
+     *
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     *
+     * @param array $attributes
+     */
+    protected function getClass(array $attributes = [])
+    {
+        return new class($attributes) extends Model {
+            use GeoLocation;
+
+            protected $table = 'customers';
+
+            protected static $unguarded = true;
+
+            public function locationAddress() : string
+            {
+                return $this->address . ' is my address';
+            }
+
+            public function fireSaving()
+            {
+                $this->fireModelEvent('saving');
+            }
+        };
+    }
+
+    /**
+     * Mocks out the expected call to the google maps api and returns tue expected
+     *
+     * @param /Illuminate\Database\Eloquent\Model $model
+     * @param string $status
+     * @param array $location
+     *
+     * @return $this
+     */
+    protected function googleApiShouldBeCalled($model, $status = 'OK', $location = [123, 234])
+    {
+        $this->getFunctionMock('TwoThirds\EloquentTraits', 'file_get_contents')
+            ->expects($this->once())
+            ->with(
+                'http://maps.google.com/maps/api/geocode/json?sensor=false&address=' . urlencode($model->locationAddress())
+            )
+            ->willReturn(json_encode([
+                'status'  => $status,
+                'results' => [['geometry' => [
+                    'location' => ['lat' => $location[0], 'lng' => $location[1]],
+                ]]],
+            ]));
+
+        return $this;
+    }
+
+    /**
+     * Asserts that the google api is not fetched
+     *
+     * @return $this
+     */
+    protected function googleApiShouldntBeCalled()
+    {
+        $this->getFunctionMock('TwoThirds\EloquentTraits', 'file_get_contents')
+            ->expects($this->never());
+
+        return $this;
+    }
+}
